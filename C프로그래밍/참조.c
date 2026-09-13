272f3ea2dfcbc1632bd07bf7c220d918abeec416
Lee-sugyeong <653waterscape@gmail.com>
2026-09-13 23:36:51 +0900

    Hello

--- C프로그래밍/참조.c
+++ C프로그래밍/참조.c
@@ -27,3 +27,32 @@ int sub(int x, int y)
 {
 	return x - y;
 }
+#include <stdio.h>
+
+int add(int, int);
+int sub(int, int);
+
+int main(void)
+{
+	int result;
+	int (*pf)(int, int);
+
+	pf = add;
+	result = pf(10, 20);
+	printf("10+20은 %d\n", result);
+
+	pf = sub;
+	result = pf(10, 20);
+	printf("10-20은 %d\n", result);
+
+	return 0;
+}
+
+int add(int x, int y)
+{
+	return x + y;
+}
+int sub(int x, int y)
+{
+	return x - y;
+}
